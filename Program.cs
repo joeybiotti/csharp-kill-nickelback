9408fbea97e46683a44cf9a97d0c96f2da2d8b29
Joey Biotti <joey@MacBook-Pro.local>
2017-07-12 15:23:48 -0500

    first commit. things work. nickelback sucks

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nickelback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string artist, string title)> goodSongs = new List<(string, string)>();
+
+            HashSet<(string artist, string title )> allSongs = new HashSet<(string, string)>();
+            allSongs.Add(("Wu-Tang Clan", "Protect Ya Neck"));
+            allSongs.Add(("The Ramones", "Beat on the Brat"));
+            allSongs.Add(("Nickleback", "Photograph"));
+            allSongs.Add(("Nickleback", "Rockstar"));
+            allSongs.Add(("Nickleback", "How You Remind Me"));
+            allSongs.Add(("Tom Waits", "Jockey Full of Bourbon"));
+            allSongs.Add(("Prince", "When Doves Cry"));
+
+            Console.WriteLine("\nThese are all the songs.\n");
+            
+            foreach(var song in allSongs)
+            {
+                Console.WriteLine($"{song.artist} by {song.title}");
+                
+                if(song.artist != "Nickleback")
+                {
+                    goodSongs.Add(song);
+                }
+            }
+
+            Console.WriteLine("\nThese are the good songs, because Nickleback sucks.\n");
+
+            foreach(var tune in goodSongs)
+            {
+                Console.WriteLine($"{tune.artist} by {tune.title}");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# C# Kill Nickleback.
+
+Lots of lists and tuples and hashsets and stuff going on here.
+
+Nickleback sucks. 
